553c4ad5f92d5e242f65c386b33a42e1e61d66a8
Indigocoder1 <caleb@liston.us>
2025-10-23 21:22:16 -0700

    Add temporary number puzzle setup

---
+++ PrototypeSubMod/Puzzles/NumberPuzzle/NumberPuzzleManager.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace PrototypeSubMod.Puzzles.NumberPuzzle;
+
+public class NumberPuzzleManager : MonoBehaviour
+{
+    [SerializeField] private Image[] displayNumbers;
+    [SerializeField] private SelectableNumber[] selectableNumbers;
+    [SerializeField] private Color deselectedColor;
+    [SerializeField] private Color selectedColor;
+
+    private int selectedIndex;
+    private int primaryNumber = -1;
+    private int secondaryNumber = -1;
+    private int previousSum = -1;
+
+    private void Start()
+    {
+        foreach (var number in displayNumbers)
+        {
+            number.color = deselectedColor;
+        }
+    }
+
+    public void SelectNumber(int number)
+    {
+        if (secondaryNumber > 0)
+        {
+            selectableNumbers[secondaryNumber - 1].EnableSelectedIndicators(0);
+        }
+
+        if (primaryNumber > 0)
+        {
+            selectableNumbers[primaryNumber - 1].EnableSelectedIndicators(0);
+        }
+        
+        if (selectedIndex == 0)
+        {
+            if (number == primaryNumber)
+            {
+                secondaryNumber = number;
+                selectedIndex++;
+            }
+            primaryNumber = number;
+        }
+        else
+        {
+            secondaryNumber = number;
+        }
+        
+        if (primaryNumber > 0)
+        {
+            int addition = primaryNumber == secondaryNumber ? 1 : 0;
+            selectableNumbers[primaryNumber - 1].EnableSelectedIndicators(1 + addition);
+        }
+        
+        if (secondaryNumber > 0 && primaryNumber != secondaryNumber)
+        {
+            selectableNumbers[secondaryNumber - 1].EnableSelectedIndicators(1);
+        }
+
+        if (primaryNumber > 0 && secondaryNumber > 0)
+        {
+            RecalculateHint();
+        }
+
+        if (selectedIndex == 1 && secondaryNumber == number && primaryNumber == number) return;
+        
+        selectedIndex = (selectedIndex + 1) % 2;
+    }
+
+    private void RecalculateHint()
+    {
+        if (previousSum != -1)
+        {
+            displayNumbers[previousSum - 1].color = deselectedColor;
+        }
+
+        int sum = primaryNumber + secondaryNumber;
+        displayNumbers[sum - 1].color = selectedColor;
+        previousSum = sum;
+    }
+}
---
+++ PrototypeSubMod/Puzzles/NumberPuzzle/SelectableNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PrototypeSubMod.Puzzles.NumberPuzzle;
+
+public class SelectableNumber : MonoBehaviour
+{
+    [SerializeField] private NumberPuzzleManager puzzleManager;
+    [SerializeField] private Image[] selectedIndicators;
+    [SerializeField] private int representativeNumber;
+
+    private void Start()
+    {
+        EnableSelectedIndicators(0);
+    }
+
+    public void Select()
+    {
+        puzzleManager.SelectNumber(representativeNumber);
+    }
+
+    public void EnableSelectedIndicators(int amount)
+    {
+        if (amount > selectedIndicators.Length)
+        {
+            throw new System.Exception($"Tried to enable more selected indicators than assigned! ({amount})");
+        }
+
+        int index = 0;
+        foreach (var indicator in selectedIndicators)
+        {
+            indicator.gameObject.SetActive(index < amount);
+            index++;
+        }
+    }
+}
