fd144dc4e1a84e0bb1d57f884302bfe1293a9ca4
Indigocoder1 <caleb@liston.us>
2025-10-25 13:49:17 -0700

    Make test wyrm movement work in 3d

--- PrototypeSubAssets/Assets/Scripts/TestRotation.cs
+++ PrototypeSubAssets/Assets/Scripts/TestRotation.cs
@@ -1,87 +1,126 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class TestRotation : MonoBehaviour
 {
     [SerializeField] private float forwardsSpeed;
+    [SerializeField] private float maxRotationSpeed;
     [SerializeField] private Transform target;
     [SerializeField] private int gizmoSteps;
     [SerializeField] private float gizmoTimestep = 1;
-    
+
+    private Vector3 upVector;
     private float rotationSpeed;
     private float angleTravelled;
     private float angleTravelledRecalculate;
 
     private void Start()
     {
-        rotationSpeed = GetRotationSpeed(transform.position, transform.right, out var angleToTarget);
-        float distToTarget = Vector3.Distance(transform.position, target.position);
+        rotationSpeed = GetRotationSpeed(transform.position, transform.forward);
+        var dirToTarget = target.position - transform.position;
+        var angleToTarget = Vector3.Angle(transform.forward, dirToTarget);
+
+        rotationSpeed = ValidateRotationSpeed(rotationSpeed, 0, out _);
         
-        angleTravelledRecalculate = Mathf.Infinity;
-        if (WithinRange(angleToTarget, 180, 0.025f))
+        if (WithinRange(angleToTarget, 180, 0.05f))
         {
-            rotationSpeed = distToTarget * 10;
-            angleTravelledRecalculate = 90;
+            rotationSpeed = -maxRotationSpeed;
+            angleTravelledRecalculate = maxRotationSpeed * 2;
         }
-        Debug.Log(rotationSpeed);
+
+        RecalculateUpVector();
     }
 
     private void Update()
     {
-        transform.position += transform.forward * (forwardsSpeed * Time.deltaTime);
-        transform.Rotate(new Vector3(0, rotationSpeed * Time.deltaTime, 0), Space.Self);
-        angleTravelled += rotationSpeed * Time.deltaTime;
-        
-        if (Mathf.Abs(angleTravelled) >= angleTravelledRecalculate)
+        transform.position += transform.forward * (forwardsSpeed * gizmoTimestep);
+        float angleDelta = rotationSpeed * gizmoTimestep;
+            
+        transform.forward = Quaternion.AngleAxis(angleDelta, upVector) * transform.forward;
+        angleTravelled += Mathf.Abs(angleDelta);
+
+        if (angleTravelled > angleTravelledRecalculate)
         {
-            rotationSpeed = GetRotationSpeed(transform.position, transform.right, out _);
-            angleTravelledRecalculate = Mathf.Infinity;
+            rotationSpeed = GetRotationSpeed(transform.position, transform.forward);
+            rotationSpeed = ValidateRotationSpeed(rotationSpeed, angleTravelled, out angleTravelledRecalculate);
         }
     }
 
     private void OnDrawGizmos()
     {
-        var rotationSpeed = GetRotationSpeed(transform.position, transform.right, out var angleToTarget);
-        float distToTarget = Vector3.Distance(transform.position, target.position);
-        float diameter = distToTarget / (Mathf.Sin(angleToTarget * Mathf.Deg2Rad) * forwardsSpeed);
-        var sign = Mathf.Sign(Vector3.Dot(target.position - transform.position, transform.right));
-        Gizmos.DrawSphere(transform.position + transform.right * diameter * forwardsSpeed / 2 * sign, 0.2f);
+        var rotSpeed = GetRotationSpeed(transform.position, transform.forward);
+        var dirToTarget = target.position - transform.position;
+        var angleToTarget = Vector3.Angle(transform.forward, dirToTarget);
         var point = transform.position;
         var direction = transform.forward;
-        
-        float angleTravelledRecalculate = Mathf.Infinity;
-        if (WithinRange(angleToTarget, 180, 0.025f))
-        {
-            rotationSpeed = distToTarget * 10;
-            angleTravelledRecalculate = 90;
-        }
 
-        float angleTravelled = 0;
+        rotSpeed = ValidateRotationSpeed(rotSpeed, 0, out var angleRecalculate);
+        
+        if (WithinRange(angleToTarget, 180, 0.05f))
+        {
+            rotSpeed = -maxRotationSpeed;
+            angleRecalculate = maxRotationSpeed * 2;
+            dirToTarget += transform.right * 0.001f;
+        }
+        
+        Gizmos.color = Color.white;
+        bool colorSwitched = false;
+        float travelledAngle = 0;
         for (int i = 0; i < gizmoSteps; i++)
         {
             Gizmos.DrawRay(point, direction * forwardsSpeed * gizmoTimestep);
             point += direction * forwardsSpeed * gizmoTimestep;
-            float angleDelta = rotationSpeed * gizmoTimestep;
-            direction = Quaternion.AngleAxis(angleDelta, transform.up) * direction;
-            angleTravelled += angleDelta;
+            float angleDelta = rotSpeed * gizmoTimestep;
+            var up = Vector3.Cross(dirToTarget, transform.forward);
             
-            if (Mathf.Abs(angleTravelled) >= angleTravelledRecalculate)
+            direction = Quaternion.AngleAxis(angleDelta, up) * direction;
+            travelledAngle += Mathf.Abs(angleDelta);
+
+            if (travelledAngle > angleRecalculate)
             {
-                Gizmos.color = Color.green;
-                rotationSpeed = GetRotationSpeed(point, -Vector3.Cross(direction, transform.up), out _);
+                rotSpeed = GetRotationSpeed(point, direction);
+                rotSpeed = ValidateRotationSpeed(rotSpeed, travelledAngle, out angleRecalculate);
+                Gizmos.color = colorSwitched ? Color.green : Color.blue;
+                colorSwitched = !colorSwitched;
             }
         }
     }
 
-    private float GetRotationSpeed(Vector3 currentPosition, Vector3 rightHandNormal, out float angleToTarget)
+    private void RecalculateUpVector()
+    {
+        var dirToTarget = target.position - transform.position;
+        var angleToTarget = Vector3.Angle(transform.forward, dirToTarget);
+        
+        if (WithinRange(angleToTarget, 180, 0.05f))
+        {
+            dirToTarget += transform.right * 0.001f;
+        }
+        
+        upVector = Vector3.Cross(dirToTarget, transform.forward);
+    }
+
+    private float ValidateRotationSpeed(float rotSpeed, float currentAngle, out float angleRecalculate)
+    {
+        angleRecalculate = Mathf.Infinity;
+
+        float speedSign = Mathf.Sign(rotSpeed);
+        float absRotSpeed = Mathf.Abs(rotSpeed);
+        if (absRotSpeed > maxRotationSpeed)
+        {
+            rotSpeed = maxRotationSpeed * 0.5f * speedSign;
+            angleRecalculate = currentAngle + maxRotationSpeed * 2;
+        }
+
+        return rotSpeed;
+    }
+
+    private float GetRotationSpeed(Vector3 currentPosition, Vector3 forward)
     {
         var dirToTarget = target.position - currentPosition;
-        angleToTarget = Vector3.Angle(transform.forward, dirToTarget);
+        var angleToTarget = Vector3.Angle(forward, dirToTarget);
         float diameter = Vector3.Distance(currentPosition, target.position) / (Mathf.Sin(angleToTarget * Mathf.Deg2Rad) * forwardsSpeed);
         var speed = 1 / (diameter * 8.74e-3f);
-        speed *= Mathf.Sign(Vector3.Dot(dirToTarget, rightHandNormal));
 
-        return speed;
+        return -speed;
     }
 
     private bool WithinRange(float value, float target, float halfRange)
