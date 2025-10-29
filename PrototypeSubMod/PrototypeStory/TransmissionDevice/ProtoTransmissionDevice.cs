66a9d27e4f0fa5268dd99747313334bd702676d7
Indigocoder1 <caleb@liston.us>
2025-10-28 18:23:47 -0700

    Add transmission device model and unity prefab

---
+++ PrototypeSubMod/PrototypeStory/TransmissionDevice/ProtoTransmissionDevice.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace PrototypeSubMod.PrototypeStory.TransmissionDevice;
+
+public class ProtoTransmissionDevice : MonoBehaviour
+{
+    [SerializeField] private GameObject poweredDownObjects;
+    [SerializeField] private GameObject poweredUpObjects;
+}
