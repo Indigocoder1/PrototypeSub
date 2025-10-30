37f6bb9ef8cff4831d3525af5f9d5d18c669a0f1
Indigocoder1 <caleb@liston.us>
2025-10-29 19:14:59 -0700

    Add transmission device activation animation

--- PrototypeSubMod/PrototypeStory/TransmissionDevice/TransmissionDeviceManager.cs
+++ PrototypeSubMod/PrototypeStory/TransmissionDevice/TransmissionDeviceManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,17 +7,37 @@ namespace PrototypeSubMod.PrototypeStory.TransmissionDevice;
 
 public class TransmissionDeviceManager : MonoBehaviour, IItemSelectorManager
 {
+    [SerializeField] private Animator animator;
     [SerializeField] private GameObject poweredDownObjects;
     [SerializeField] private GameObject poweredUpObjects;
+    [SerializeField] private float activationDelay;
+
+    private bool deployed;
+    
+    private void Start()
+    {
+        var pickupable = GetComponent<Pickupable>();
+        
+        if (!pickupable) deployed = true;
+    }
 
     public void OnHandHover(HandTargetEventData data)
     {
+        if (!deployed)
+        {
+            HandReticle.main.SetText(HandReticle.TextType.Hand,"Deploy from prototype to use", true);
+            HandReticle.main.SetIcon(HandReticle.IconType.HandDeny);
+            return;
+        }
+        
         HandReticle.main.SetText(HandReticle.TextType.Hand,"Insert cash or select payment type", true, GameInput.Button.LeftHand);
         HandReticle.main.SetIcon(HandReticle.IconType.Hand);
     }
     
     public void OnHandClick(HandTargetEventData data)
     {
+        if (!deployed) return;
+        
         uGUI.main.itemSelector.Initialize(this, SpriteManager.Get(SpriteManager.Group.Item, "nobattery"), new List<IItemsContainer>
         {
             Inventory.main.container
@@ -60,9 +81,28 @@ public class TransmissionDeviceManager : MonoBehaviour, IItemSelectorManager
         if (!Inventory.main.TryRemoveItem(item.item)) throw new Exception($"Could not remove {item.item} from inventory");
 
         Destroy(item.item.gameObject);
+
+        StartCoroutine(ActivateDevice());
+    }
+
+    private IEnumerator ActivateDevice()
+    {
+        animator.SetTrigger("Activate");
+        yield return new WaitForSeconds(activationDelay);
         
         ErrorMessage.AddDebug("Powered up transmission device");
         poweredDownObjects.SetActive(false);
         poweredUpObjects.SetActive(true);
+    }
+
+    public void DeployDevice()
+    {
+        var pickupable = GetComponent<Pickupable>();
+        if (pickupable)
+        {
+            Destroy(pickupable);
+        }
+
+        deployed = true;
     }
 }
