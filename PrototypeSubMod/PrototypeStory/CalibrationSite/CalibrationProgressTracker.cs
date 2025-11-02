485f8275442c1975fa536de17342549d48cf6b79
Indigocoder1 <caleb@liston.us>
2025-11-02 12:55:16 -0800

    Add in calibration test run logic

---
+++ PrototypeSubMod/PrototypeStory/CalibrationSite/CalibrationProgressTracker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+namespace PrototypeSubMod.PrototypeStory.CalibrationSite;
+
+public class CalibrationProgressTracker : MonoBehaviour, IScheduledUpdateBehaviour
+{
+    [SerializeField] private CalibrationRunManager runManager;
+    [SerializeField] private Transform subIcon;
+    [SerializeField] private Transform[] calibrationPoints;
+    
+    public string GetProfileTag() => "CalibrationRunProgressTracker";
+    public int scheduledUpdateIndex { get; set; }
+    
+    public void ScheduledUpdate()
+    {
+        int nextIndex = runManager.GetNextPointIndex();
+        Vector3 pointA = runManager.GetCalibrationPoint(nextIndex - 1);
+        Vector3 pointB = runManager.GetCalibrationPoint(nextIndex);
+        float progress = InverseLerp(pointA, pointB, transform.position);
+
+        subIcon.position = Vector3.Lerp(calibrationPoints[nextIndex - 1].position, calibrationPoints[nextIndex].position, progress);
+    }
+    
+    private float InverseLerp(Vector3 a, Vector3 b, Vector3 value)
+    {
+        Vector3 AB = b - a;
+        Vector3 AV = value - a;
+        return Vector3.Dot(AV, AB) / Vector3.Dot(AB, AB);
+    }
+
+    public virtual void OnEnable()
+    {
+        UpdateSchedulerUtils.Register(this);
+    }
+
+    public virtual void OnDisable()
+    {
+        UpdateSchedulerUtils.Deregister(this);
+    }
+}
---
+++ PrototypeSubMod/PrototypeStory/CalibrationSite/CalibrationRunManager.cs
@@ -0,0 +1,59 @@
+﻿using Nautilus.Utility;
+using UnityEngine;
+
+namespace PrototypeSubMod.PrototypeStory.CalibrationSite;
+
+public class CalibrationRunManager : MonoBehaviour
+{
+    private static readonly Vector3 InitialPoint = new(-2220, -390, 420);
+
+    [SerializeField] private float globalSpacing = 100;
+    [SerializeField] private float[] pointSpacings;
+    [SerializeField] private float[] relativePointAngles;
+    [SerializeField] private float distToCountAsReached = 10;
+
+    private int nextPointIndex = 1;
+    private Vector3[] calibrationPoints;
+
+    private void Start()
+    {
+        // Add one to account for initial point
+        calibrationPoints = new Vector3[relativePointAngles.Length + 1];
+        calibrationPoints[0] = InitialPoint;
+        for (int i = 0; i < relativePointAngles.Length; i++)
+        {
+            var xComponent = Mathf.Cos(relativePointAngles[i] * Mathf.Deg2Rad);
+            var yComponent = Mathf.Sin(relativePointAngles[i] * Mathf.Deg2Rad);
+            var offset = new Vector3(xComponent, 0, yComponent) * pointSpacings[i] * globalSpacing;
+            calibrationPoints[i + 1] = calibrationPoints[i] + offset;
+        }
+
+        foreach (var point in calibrationPoints)
+        {
+            var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            MaterialUtils.ApplySNShaders(sphere);
+            sphere.transform.position = point;
+            sphere.transform.localScale = Vector3.one * 10f;
+            Destroy(sphere.GetComponent<Collider>());
+        }
+    }
+    
+    private void Update()
+    {
+        var dist = Vector3.Distance(transform.position, calibrationPoints[nextPointIndex]);
+        Plugin.Logger.LogInfo($"Dist = {dist}");
+        if (dist > distToCountAsReached) return;
+        
+        ErrorMessage.AddError($"Reached point {nextPointIndex}");
+        nextPointIndex++;
+
+        if (nextPointIndex >= calibrationPoints.Length)
+        {
+            ErrorMessage.AddError("Calibration complete");
+            nextPointIndex = calibrationPoints.Length - 1;
+        }
+    }
+
+    public int GetNextPointIndex() => nextPointIndex;
+    public Vector3 GetCalibrationPoint(int index) => calibrationPoints[index];
+}
