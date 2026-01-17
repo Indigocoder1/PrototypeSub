04e969d0985073c520a0da03c996639075c40bf4
Indigocoder1 <caleb@liston.us>
2026-01-17 14:26:14 -0800

    Add number puzzle ping after getting radio 1

---
+++ PrototypeSubMod/Registration/PuzzleHintRegistration.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using Nautilus.Handlers;
+using PrototypeSubMod.Prefabs.FacilityProps;
+using UnityEngine;
+
+namespace PrototypeSubMod.Registration;
+
+public static class PuzzleHintRegistration
+{
+    public static void Register()
+    {
+        #region Number Puzzle Ping
+
+        StoryGoalHandler.RegisterCustomEvent("OnPlayProtoRadioMessage1", () =>
+        {
+            var numberPuzzlePingPos = new Vector3(-238f, -68f, 290f);
+            var techType = CustomPing.CreatePing("NumberPuzzlePing", PingType.Signal);
+            UWE.CoroutineHost.StartCoroutine(SpawnPrefab(techType, numberPuzzlePingPos));
+        });
+        #endregion
+    }
+
+    private static IEnumerator SpawnPrefab(TechType techType, Vector3 position)
+    {
+        var task = CraftData.GetPrefabForTechTypeAsync(techType);
+        yield return task;
+
+        GameObject.Instantiate(task.GetResult(), position, Quaternion.identity);
+    }
+}
