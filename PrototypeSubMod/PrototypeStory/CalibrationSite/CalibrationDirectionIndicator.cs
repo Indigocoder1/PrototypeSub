d697d0993e149d471bb80dbf82a0214e4dbbcb10
Indigocoder1 <caleb@liston.us>
2026-01-19 10:34:33 -0800

    Refactor calibration direction indicator sprites

--- PrototypeSubMod/PrototypeStory/CalibrationSite/CalibrationDirectionIndicator.cs
+++ PrototypeSubMod/PrototypeStory/CalibrationSite/CalibrationDirectionIndicator.cs
@@ -1,68 +1,31 @@
-﻿using System;
+﻿using PrototypeSubMod.Puzzles.BearingPuzzle;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace PrototypeSubMod.PrototypeStory.CalibrationSite;
 
 public class CalibrationDirectionIndicator : MonoBehaviour
 {
-    [SerializeField] private Image headingSprite;
-    [SerializeField] private Image backgroundSprite;
-    [SerializeField] private Transform leftHalfMask;
-    [SerializeField] private Transform rightHalfMask;
+    [SerializeField] private Transform spriteParent;
     [SerializeField] private CalibrationRunManager runManager;
-    [SerializeField] private CompassDirection[] compassDirections;
-
-    [HideInInspector, SerializeField]
-    public Sprite[] headingSprites;
-    [HideInInspector, SerializeField]
-    public bool[] leftHalves;
-
-    private CompassDirection[] serializedDirections;
-    
-    private void OnValidate()
-    {
-        headingSprites = new Sprite[compassDirections.Length];
-        leftHalves = new bool[compassDirections.Length];
-        for (int i = 0; i < compassDirections.Length; i++)
-        {
-            headingSprites[i] = compassDirections[i].heading;
-            leftHalves[i] = compassDirections[i].isLeftHalf;
-        }
-    }
+    [SerializeField] private BearingReferenceSymbol[] bearingReferenceSymbols;
 
     private void Start()
     {
-        serializedDirections = new CompassDirection[headingSprites.Length];
-        for (int i = 0; i < headingSprites.Length; i++)
-        {
-            serializedDirections[i] = new CompassDirection(headingSprites[i], leftHalves[i]);
-        }
-
         runManager.onPointReached += OnPointReached;
     }
 
     private void OnPointReached(int index)
     {
-        if (index < 0 || index > serializedDirections.Length - 1) return;
+        if (index < 0 || index > bearingReferenceSymbols.Length - 1) return;
 
-        headingSprite.sprite = serializedDirections[index].heading;
-        bool onLeft = serializedDirections[index].isLeftHalf;
+        foreach (Transform child in spriteParent)
+        {
+            Destroy(child.gameObject);
+        }
 
-        headingSprite.transform.SetParent(onLeft ? leftHalfMask : rightHalfMask);
-        backgroundSprite.transform.SetParent(onLeft ? rightHalfMask : leftHalfMask);
+        var symbolObject = bearingReferenceSymbols[index].CreateSymbolObject();
+        symbolObject.transform.SetParent(spriteParent, false);
     }
-}
 
-[Serializable]
-public struct CompassDirection
-{
-    public Sprite heading;
-    public bool isLeftHalf;
-
-    public CompassDirection(Sprite heading, bool isLeftHalf)
-    {
-        this.heading = heading;
-        this.isLeftHalf = isLeftHalf;
-    }
+    public BearingReferenceSymbol[] GetBearingReferenceSymbols() => bearingReferenceSymbols;
 }
