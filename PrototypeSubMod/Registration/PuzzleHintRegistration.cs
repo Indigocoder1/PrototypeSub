49c41286f24ef424ea734b569d190a6be5e1e43e
Indigocoder1 <caleb@liston.us>
2026-01-19 13:24:14 -0800

    Add calibration site ping upon 3rd radio message

--- PrototypeSubMod/Registration/PuzzleHintRegistration.cs
+++ PrototypeSubMod/Registration/PuzzleHintRegistration.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using Nautilus.Handlers;
 using PrototypeSubMod.Prefabs.FacilityProps;
+using PrototypeSubMod.PrototypeStory.CalibrationSite;
 using UnityEngine;
 
 namespace PrototypeSubMod.Registration;
@@ -10,20 +11,28 @@ public static class PuzzleHintRegistration
     public static void Register()
     {
         #region Number Puzzle Ping
+        var numberPuzzlePingTechType = CustomPing.CreatePing("NumberPuzzlePing", PingType.Signal);
         StoryGoalHandler.RegisterCustomEvent("OnPlayProtoRadioMessage1", () =>
         {
             var numberPuzzlePingPos = new Vector3(-238f, -68f, 290f);
-            var techType = CustomPing.CreatePing("NumberPuzzlePing", PingType.Signal);
-            UWE.CoroutineHost.StartCoroutine(SpawnPrefab(techType, numberPuzzlePingPos));
+            UWE.CoroutineHost.StartCoroutine(SpawnPrefab(numberPuzzlePingTechType, numberPuzzlePingPos));
         });
         #endregion
         
         #region Bearing Puzzle Ping
+        var bearingPingTechType = CustomPing.CreatePing("BearingPuzzlePing", PingType.Signal);
         StoryGoalHandler.RegisterCustomEvent("OnPlayProtoRadioMessage2", () =>
         {
             var bearingPuzzlePingPos = new Vector3(1226, -306, 534);
-            var techType = CustomPing.CreatePing("BearingPuzzlePing", PingType.Signal);
-            UWE.CoroutineHost.StartCoroutine(SpawnPrefab(techType, bearingPuzzlePingPos));
+            UWE.CoroutineHost.StartCoroutine(SpawnPrefab(bearingPingTechType, bearingPuzzlePingPos));
+        });
+        #endregion
+        
+        #region Calibration Site Ping
+        var calibrationPingTechType = CustomPing.CreatePing("CalibrationSitePing", PingType.Signal);
+        StoryGoalHandler.RegisterCustomEvent("OnPlayProtoRadioMessage3", () =>
+        {
+            UWE.CoroutineHost.StartCoroutine(SpawnPrefab(calibrationPingTechType, CalibrationRunManager.InitialPoint));
         });
         #endregion
     }
