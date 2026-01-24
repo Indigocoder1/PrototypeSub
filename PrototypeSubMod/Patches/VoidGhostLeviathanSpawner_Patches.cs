2e787fee6cd0a52ee6315859ded510e6c25c9432
OctolinkG <159560650+OctolinkG@users.noreply.github.com>
2026-01-24 15:48:29 +0100

    Remove ghost leviathan void spawns after wyrm activation

---
+++ PrototypeSubMod/Patches/VoidGhostLeviathanSpawner_Patches.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using Nautilus.Handlers;
+using Story;
+using UnityEngine;
+using UWE;
+
+namespace PrototypeSubMod.Patches;
+
+[HarmonyPatch(typeof(VoidGhostLeviathansSpawner))]
+public class VoidGhostLeviathanSpawnerPatch
+{
+    [HarmonyPatch(nameof(VoidGhostLeviathansSpawner.IsPlayerInVoid))]
+    [HarmonyPrefix]
+    public static bool CheckWormActivationGoal()
+    {
+        StoryGoalManager storyGoalManager = StoryGoalManager.main;
+        if (storyGoalManager.IsGoalComplete("HullFacilityActivateWorm"))
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
