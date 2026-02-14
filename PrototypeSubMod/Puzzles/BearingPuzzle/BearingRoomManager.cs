24fe860986ec388afa8a01b360b889e93a5352d3
Indigocoder1 <caleb@liston.us>
2026-02-14 10:45:41 -0800

    Add SFX to bearing puzzle confirmation

--- PrototypeSubMod/Puzzles/BearingPuzzle/BearingRoomManager.cs
+++ PrototypeSubMod/Puzzles/BearingPuzzle/BearingRoomManager.cs
@@ -8,17 +8,22 @@ public class BearingRoomManager : MonoBehaviour
 {
     [SerializeField] private CyclingBearingIndicator[] cyclingIndicators;
     [SerializeField] private UnityEvent onRoomComplete;
-
+    [SerializeField] private FMODAsset correctSequenceSfx;
+    [SerializeField] private FMODAsset incorrectSequenceSfx;
+    [SerializeField] private float sfxVolume = 1;
+    
     public void OnConfirmationClicked()
     {
         if (HasCorrectSequence())
         {
             ErrorMessage.AddError(Language.main.Get("ProtoPuzzleCorrectSequence"));
             onRoomComplete?.Invoke();
+            FMODUWE.PlayOneShot(correctSequenceSfx, Player.main.transform.position, sfxVolume);
         }
         else
         {
             ErrorMessage.AddError(Language.main.Get("ProtoPuzzleIncorrectSequence"));
+            FMODUWE.PlayOneShot(incorrectSequenceSfx, Player.main.transform.position, sfxVolume);
         }
     }
 
