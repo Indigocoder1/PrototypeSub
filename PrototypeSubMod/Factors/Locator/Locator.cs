ad2e73304fd8543e97636020c70f70dc232aed79
Limitless <impossarchi@gmail.com>
2026-02-17 00:10:54 -0800

    Switch Locator to passive-with-toggle, simple play at facility locations

--- PrototypeSubMod/Factors/Locator/Locator.cs
+++ PrototypeSubMod/Factors/Locator/Locator.cs
@@ -21,10 +21,15 @@ namespace PrototypeSubMod.Factors.Locator;
 
 public class Locator : Factor
 {
+    private bool toggle = true;
+    private const float distanceUnit = 200f;
+    private float timeLastPing;
+    [SerializeField] private FMODAsset pingSfxAsset;
+
     public Locator()
     {
-        cooldown = 1f;
-        duration = 10.7188f; // keep synced with actual length of `FacilityDetectionPing.wav` in seconds
+        // cooldown = 1f;
+        duration = 7.0759f; // keep synced with actual length of `FacilityDetectionPing.wav` in seconds
     }
 
     private GameObject locatorSource;
@@ -38,31 +43,32 @@ public class Locator : Factor
     public override void OnEquipped()
     {
         base.OnEquipped();
-        if (locatorSource is null)
-            UWE.CoroutineHost.StartCoroutine(SpawnLocatorSource());
-        else
-            locatorSource.SetActive(true);
+        timeLastPing = Time.time - duration;
+        // if (locatorSource is null)
+        //     UWE.CoroutineHost.StartCoroutine(SpawnLocatorSource());
+        // else
+        //     locatorSource.SetActive(true);
     }
 
     public override void OnUnequipped()
     {
         base.OnUnequipped();
-        locatorSource?.SetActive(false);
+        // locatorSource?.SetActive(false);
     }
 
-    private IEnumerator SpawnLocatorSource()
-    {
-        var prefabTask = CraftData.GetPrefabForTechTypeAsync(LocatorFactorSource.prefabInfo.TechType);
-        yield return prefabTask;
-        var prefab = prefabTask.GetResult();
-        locatorSource = Instantiate(prefab);
-        locatorSource.transform.SetParent(Player.main.transform);
-        locatorSource.transform.localPosition = new Vector3(0, 0, 0);
-        Debug.Log($"Locator source spawned. Parent: {locatorSource.transform.parent.name}");
-        var hasComponent = locatorSource.TryGetComponent<LocatorSourceManager>(out var lsm);
-        Debug.Log($"Locator source has LocatorSourceManager component: {hasComponent}");
-        lsm.locatorFactor = this;
-    }
+    // private IEnumerator SpawnLocatorSource()
+    // {
+    //     var prefabTask = CraftData.GetPrefabForTechTypeAsync(LocatorFactorSource.prefabInfo.TechType);
+    //     yield return prefabTask;
+    //     var prefab = prefabTask.GetResult();
+    //     locatorSource = Instantiate(prefab);
+    //     locatorSource.transform.SetParent(Player.main.transform);
+    //     locatorSource.transform.localPosition = new Vector3(0, 0, 0);
+    //     Debug.Log($"Locator source spawned. Parent: {locatorSource.transform.parent.name}");
+    //     var hasComponent = locatorSource.TryGetComponent<LocatorSourceManager>(out var lsm);
+    //     Debug.Log($"Locator source has LocatorSourceManager component: {hasComponent}");
+    //     lsm.locatorFactor = this;
+    // }
 
     // private GameObject CreateLocatorEffect()
     // {
@@ -79,18 +85,32 @@ public class Locator : Factor
     public override void StartUse()
     {
         base.StartUse();
-        if (locatorSource is null) UWE.CoroutineHost.StartCoroutine(SpawnLocatorSource());
-        locatorSource?.GetComponent<LocatorSourceManager>().StartUse();
+        // Actually called every time the use button is pressed, so toggle
+        toggle = !toggle;
+        // if (locatorSource is null) UWE.CoroutineHost.StartCoroutine(SpawnLocatorSource());
+        // locatorSource?.SetActive(true);
+        // locatorSource?.GetComponent<LocatorSourceManager>().StartUse();
     }
 
-//   public override void UpdateFactor()
-//   {
-//     base.UpdateFactor();
-//   }
-
-    public override void StopUse()
+    public override void UpdateFactor()
     {
-        // locatorSource?.GetComponent<LocatorSourceManager>().GracefulStop();
-        base.StopUse();
+        base.UpdateFactor();
+        if (!toggle || Time.time - timeLastPing < duration) return;
+        timeLastPing = Time.time;
+        
+        var loc = Player.main.transform.position;
+        (string name, Vector3 pos, float dist) = Plugin.FACILITY_POSITIONS
+            .Select(kv => (kv.Key, kv.Value, dist: Vector3.Distance(loc, kv.Value)))
+            .OrderBy(t => t.dist)
+            .FirstOrFallback(("Player", loc, 0f));
+        if (dist == 0f) return;
+        Utils.PlayFMODAsset(pingSfxAsset, pos);
     }
+
+    // public override void StopUse()
+    // {
+    //     // locatorSource?.GetComponent<LocatorSourceManager>().GracefulStop();
+    //     base.StopUse();
+    //     // locatorSource?.SetActive(false);
+    // }
 }
