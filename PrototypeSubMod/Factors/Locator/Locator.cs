d0cd647858413b443aa0d8f0cbb60a0e7e024598
OctolinkG <159560650+OctolinkG@users.noreply.github.com>
2026-02-17 19:02:30 +0100

    Add locator factor minimum distance and tooltips

--- PrototypeSubMod/Factors/Locator/Locator.cs
+++ PrototypeSubMod/Factors/Locator/Locator.cs
@@ -22,64 +22,28 @@ namespace PrototypeSubMod.Factors.Locator;
 public class Locator : Factor
 {
     private bool toggle = true;
-    private const float distanceUnit = 200f;
     private float timeLastPing;
+    [SerializeField] private int maxPingDistance = 300;
     [SerializeField] private FMODAsset pingSfxAsset;
 
     public Locator()
     {
-        // cooldown = 1f;
         duration = 7.0759f; // keep synced with actual length of `FacilityDetectionPing.wav` in seconds
     }
 
     private GameObject locatorSource;
-
-    // private void Awake()
-    // {
-    //     pingSfxAsset = AudioUtils.GetFmodAsset("event:/sub/cyclops/ping");
-    // }
 
     public override GameInput.Button GetUseButton() => InputRegisterer.LocatorButton;
     public override void OnEquipped()
     {
         base.OnEquipped();
         timeLastPing = Time.time - duration;
-        // if (locatorSource is null)
-        //     UWE.CoroutineHost.StartCoroutine(SpawnLocatorSource());
-        // else
-        //     locatorSource.SetActive(true);
     }
 
     public override void OnUnequipped()
     {
         base.OnUnequipped();
-        // locatorSource?.SetActive(false);
     }
-
-    // private IEnumerator SpawnLocatorSource()
-    // {
-    //     var prefabTask = CraftData.GetPrefabForTechTypeAsync(LocatorFactorSource.prefabInfo.TechType);
-    //     yield return prefabTask;
-    //     var prefab = prefabTask.GetResult();
-    //     locatorSource = Instantiate(prefab);
-    //     locatorSource.transform.SetParent(Player.main.transform);
-    //     locatorSource.transform.localPosition = new Vector3(0, 0, 0);
-    //     Debug.Log($"Locator source spawned. Parent: {locatorSource.transform.parent.name}");
-    //     var hasComponent = locatorSource.TryGetComponent<LocatorSourceManager>(out var lsm);
-    //     Debug.Log($"Locator source has LocatorSourceManager component: {hasComponent}");
-    //     lsm.locatorFactor = this;
-    // }
-
-    // private GameObject CreateLocatorEffect()
-    // {
-    //     var locatorSource = new GameObject("LocatorSource");
-    //     locatorSource.transform.SetParent(Player.main.transform);
-    //     locatorSource.transform.localPosition = new Vector3(0, 0, 0);
-    //     srcSfx = locatorSource.AddComponent<FMOD_CustomEmitter>();
-    //     foreach (var field in typeof(FMOD_CustomEmitter).GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
-    //         field.SetValue(srcSfx, field.GetValue(pingSfx));
-    //     return locatorSource;
-    // }
 
 
     public override void StartUse()
@@ -87,9 +51,17 @@ public class Locator : Factor
         base.StartUse();
         // Actually called every time the use button is pressed, so toggle
         toggle = !toggle;
-        // if (locatorSource is null) UWE.CoroutineHost.StartCoroutine(SpawnLocatorSource());
-        // locatorSource?.SetActive(true);
-        // locatorSource?.GetComponent<LocatorSourceManager>().StartUse();
+
+        if (toggle)
+        {
+            FMODUWE.PlayOneShot(AudioUtils.GetFmodAsset("ButtonSelect"), Player.main.transform.position);
+            ErrorMessage.AddError("Listening device active. Press again to toggle.");
+        }
+         else
+        {
+            FMODUWE.PlayOneShot(AudioUtils.GetFmodAsset("ButtonBack"), Player.main.transform.position);
+            ErrorMessage.AddError("Listening device deactivated. Press again to toggle.");
+        }
     }
 
     public override void UpdateFactor()
@@ -99,18 +71,15 @@ public class Locator : Factor
         timeLastPing = Time.time;
         
         var loc = Player.main.transform.position;
+
         (string name, Vector3 pos, float dist) = Plugin.FACILITY_POSITIONS
             .Select(kv => (kv.Key, kv.Value, dist: Vector3.Distance(loc, kv.Value)))
             .OrderBy(t => t.dist)
             .FirstOrFallback(("Player", loc, 0f));
+
+        // maxPingDistance should be about equal to the FMOD asset's 3D max distance to prevent popping sound, but I can't reference for some reason, so just hardcode it here for now -Chris
+        if (dist > maxPingDistance) return;
         if (dist == 0f) return;
         Utils.PlayFMODAsset(pingSfxAsset, pos);
     }
-
-    // public override void StopUse()
-    // {
-    //     // locatorSource?.GetComponent<LocatorSourceManager>().GracefulStop();
-    //     base.StopUse();
-    //     // locatorSource?.SetActive(false);
-    // }
 }
