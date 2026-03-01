310b206fec0bc0db54a549e89fa2db806a2f31f9
Indigocoder1 <caleb@liston.us>
2026-03-01 08:13:41 -0800

    Add new voicelines to voiceline registerer

--- PrototypeSubMod/Registration/VoicelineRegisterer.cs
+++ PrototypeSubMod/Registration/VoicelineRegisterer.cs
@@ -66,6 +66,8 @@ public static class VoicelineRegisterer
 
         "Proto_OnTransmissionDeviceFirstLoaded",
         "Proto_OnCalibrationRunCompleted",
+        "Proto_CalibrationPointReached",
+        "Proto_CalibrationTestFailed",
         "Proto_StoryEndPingVoiceline",
         "Proto_AdminFacilityOnline",
         "Proto_DefensePingSpawned",
