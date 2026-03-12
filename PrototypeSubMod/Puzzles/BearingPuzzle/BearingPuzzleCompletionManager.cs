bdd1fb543d0b10360408dd2dea9dc1cfd7c4acf6
OctolinkG <159560650+OctolinkG@users.noreply.github.com>
2026-03-13 00:36:17 +0100

    Make bearing puzzle lights disable on factor terminal interact

---
+++ PrototypeSubMod/Puzzles/BearingPuzzle/BearingPuzzleCompletionManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using PrototypeSubMod.MiscMonobehaviors;
+using PrototypeSubMod.Puzzles.NumberPuzzle;
+using PrototypeSubMod.Registration;
+using Story;
+using UnityEngine;
+
+namespace PrototypeSubMod.Puzzles.BearingPuzzle;
+
+public class BearingPuzzleCompletionManager : MonoBehaviour
+{
+    [SerializeField] private LightingController lightingController;
+    [SerializeField] private SequencedLightEnabler sequencedLightEnabler;
+
+    private void Start()
+    {
+        if (!StoryGoalManager.main.IsGoalComplete("ProtoBearingPuzzleComplete")) return;
+
+        OnPuzzleCompleted();
+        lightingController.SnapToState(2);
+        sequencedLightEnabler.ActivateLightsSequentially();
+    }
+
+    private void OnPuzzleCompleted()
+    {
+        StoryGoalManager.main.OnGoalComplete("ProtoBearingPuzzleComplete");
+    }
+
+    public void OnFactorDownloaded()
+    {
+        UWE.CoroutineHost.StartCoroutine(DisableLightsDelayed());
+        ErrorMessage.AddError("Disabling lights!");
+    }
+
+    private IEnumerator DisableLightsDelayed()
+    {
+        yield return new WaitForSeconds(6.6f);
+        lightingController.LerpToState(2);
+        sequencedLightEnabler.ActivateLightsSequentially();
+    }
+}
