2531f6a44bd023b1371ae53ec44f3fc4127d46eb
Indigocoder1 <caleb@liston.us>
2026-03-29 16:51:18 -0700

    Fix color factor with the prototype looking weird

---
+++ PrototypeSubAssets/Assets/Scripts/MaterialSwapper.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class MaterialSwapper : MonoBehaviour
+{
+    [SerializeField] private Material materialFrom;
+    [SerializeField] private Material materialTo;
+    [SerializeField] private bool swapMaterials;
+    
+    private void OnDrawGizmosSelected()
+    {
+        if (!swapMaterials) return;
+        swapMaterials = false;
+
+        foreach (var rend in GetComponentsInChildren<Renderer>(true))
+        {
+            var materials = rend.sharedMaterials;
+            for (var i = 0; i < materials.Length; i++)
+            {
+                if (materials[i] == materialFrom)
+                {
+                    materials[i] = materialTo;
+                }
+            }
+
+            rend.sharedMaterials = materials;
+        }
+    }
+}
