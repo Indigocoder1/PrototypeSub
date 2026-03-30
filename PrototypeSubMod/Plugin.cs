b32fb562268af0a2abb1e15534a7f4e0ef1ab57a
Indigocoder1 <caleb@liston.us>
2026-03-30 14:38:02 -0700

    Increase Nautilus dependency version

--- PrototypeSubMod/Plugin.cs
+++ PrototypeSubMod/Plugin.cs
@@ -39,7 +39,7 @@ using Nautilus.FMod;
 namespace PrototypeSubMod
 {
     [BepInPlugin(GUID, pluginName, versionString)]
-    [BepInDependency("com.snmodding.nautilus", "1.0.0.47")]
+    [BepInDependency("com.snmodding.nautilus", "1.0.0.49")]
     [BepInDependency("com.indigocoder.sublibrary", "1.7.5")]
     [BepInDependency("Esper89.TerrainPatcher", "1.2.2")]
     [BepInDependency("Indigocoder.SuitLib", "1.1.8")]
