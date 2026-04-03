e79e6228a4f132c7f4cac58a80e13cba28f6791e
Indigocoder1 <caleb@liston.us>
2026-04-03 15:56:29 -0700

    Add whitelisted prefabs for spawning in facilities

--- PrototypeSubMod/Patches/PrefabPlaceholder_Patches.cs
+++ PrototypeSubMod/Patches/PrefabPlaceholder_Patches.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using System.Threading;
+﻿using System.Collections.Generic;
 using HarmonyLib;
 using UnityEngine;
 
@@ -8,9 +7,17 @@ namespace PrototypeSubMod.Patches;
 [HarmonyPatch(typeof(PrefabPlaceholder))]
 public static class PrefabPlaceholder_Patches
 {
+    private static readonly List<string> WhitelistedClassIDs = new()
+    {
+        "38ebd2e5-9dcc-4d7a-ada4-86a22e01191a", // Ion cubes
+        "41406e76-4b4c-4072-86f8-f5b8e6523b73" // Drillable ion cubes
+    };
+    
     [HarmonyPatch(nameof(PrefabPlaceholder.Spawn)), HarmonyPrefix]
     private static bool Spawn_Prefix(PrefabPlaceholder __instance)
     {
+        if (WhitelistedClassIDs.Contains(__instance.prefabClassId)) return true;
+        
         var testPos = __instance.transform.position;
         
         return !InFacilityBiome(testPos);
