d8e41e461168a4ea4c7513aa8d9d3cb44b130dcc
Indigocoder1 <caleb@liston.us>
2026-04-16 17:37:28 -0700

    Add sub position indicator to the interceptor map

---
+++ PrototypeSubMod/Teleporter/MapPositionIndicator.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace PrototypeSubMod.Teleporter;
+
+public class MapPositionIndicator : MonoBehaviour, IScheduledUpdateBehaviour
+{
+    [SerializeField] private SubRoot subRoot;
+    [SerializeField] private Transform[] subIndicators;
+    [SerializeField] private float positionScaleFactor;
+    [SerializeField] private float maxDistanceFromCenter;
+
+    public string GetProfileTag() => "MapPositionIndicator";
+
+    public void ScheduledUpdate()
+    {
+        Vector3 scaledPos = subRoot.transform.position * positionScaleFactor;
+        var tooFarFromCenter = Vector2.Distance(Vector2.zero, new Vector2(scaledPos.x, scaledPos.z)) > maxDistanceFromCenter;
+        foreach (var indicator in subIndicators)
+        {
+            indicator.gameObject.SetActive(!tooFarFromCenter);
+            if (tooFarFromCenter) continue;
+            
+            indicator.localPosition = new Vector3(scaledPos.x, scaledPos.z, indicator.localPosition.z);
+            indicator.localEulerAngles = new Vector3(0, 0, 360 - subRoot.transform.localEulerAngles.y);
+        }
+    }
+
+    public int scheduledUpdateIndex { get; set; }
+
+    private void OnEnable()
+    {
+        UpdateSchedulerUtils.Register(this);
+    }
+    
+    private void OnDisable()
+    {
+        UpdateSchedulerUtils.Deregister(this);
+    }
+}
