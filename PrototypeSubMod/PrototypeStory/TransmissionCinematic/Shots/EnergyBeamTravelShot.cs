de0d539d4d8b2d6d586ef86dfe96a964f212000f
Indigocoder1 <caleb@liston.us>
2026-05-17 13:46:24 -0700

    Adjust energy beam travel effects

--- PrototypeSubMod/PrototypeStory/TransmissionCinematic/Shots/EnergyBeamTravelShot.cs
+++ PrototypeSubMod/PrototypeStory/TransmissionCinematic/Shots/EnergyBeamTravelShot.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using PrototypeSubMod.MiscMonobehaviors;
+using UnityEngine;
 
 namespace PrototypeSubMod.PrototypeStory.TransmissionCinematic.Shots;
 
 public class EnergyBeamShot : CinematicShot
 {
     public override event Action<DeviceCinematicManager> OnShotCompleted;
+
+    [SerializeField] private PostProcessOverride defaultPostProcessing;
+
+    public override void PlayShot(Animator animator, DeviceCinematicManager cinematicManager)
+    {
+        base.PlayShot(animator, cinematicManager);
+        defaultPostProcessing.ApplyOverrides();
+    }
 
     public void EndEnergyBeamShot()
     {
