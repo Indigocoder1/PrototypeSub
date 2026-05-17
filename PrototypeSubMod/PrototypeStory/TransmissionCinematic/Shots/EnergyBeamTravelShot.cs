77491d4431611a6c3b0955c142ffddd3824f882c
Indigocoder1 <caleb@liston.us>
2026-05-17 12:53:20 -0700

    Add energy beam travel shot

---
+++ PrototypeSubMod/PrototypeStory/TransmissionCinematic/Shots/EnergyBeamTravelShot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PrototypeSubMod.PrototypeStory.TransmissionCinematic.Shots;
+
+public class EnergyBeamShot : CinematicShot
+{
+    public override event Action<DeviceCinematicManager> OnShotCompleted;
+
+    public void EndEnergyBeamShot()
+    {
+        OnShotCompleted?.Invoke(deviceCinematicManager);
+    }
+}
