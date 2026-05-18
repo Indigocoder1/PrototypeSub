24c64a0c187788583f96f0010cf26873f3348d37
Indigocoder1 <130301845+Indigocoder1@users.noreply.github.com>
2026-05-18 07:17:38 -0700

    Add beginning of beam dissipate shot

---
+++ PrototypeSubMod/PrototypeStory/TransmissionCinematic/Shots/BeamDissipateShot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace PrototypeSubMod.PrototypeStory.TransmissionCinematic.Shots;
+
+public class BeamDissipateShot : CinematicShot
+{
+    public override event Action<DeviceCinematicManager> OnShotCompleted;
+
+    [SerializeField] private QepLaserSpawner laserSpawner;
+
+    public override void PlayShot(Animator animator, DeviceCinematicManager cinematicManager)
+    {
+        base.PlayShot(animator, cinematicManager);
+        laserSpawner.PlayLaserVFX();
+    }
+}
