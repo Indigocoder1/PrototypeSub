4310680bd8fdf17f52f56e2a8cb722e3777f7f1e
Indigocoder1 <130301845+Indigocoder1@users.noreply.github.com>
2026-05-18 07:26:47 -0700

    Move sun lens flare in front of planet

---
+++ PrototypeSubAssets/Assets/Scripts/MoveAlongLine.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class MoveAlongLine : MonoBehaviour
+{
+    [SerializeField] private Transform pointA;
+    [SerializeField] private Transform pointB;
+    [SerializeField] private Transform movingPoint;
+
+    private void OnDrawGizmosSelected()
+    {
+        if (!pointA || !pointB || !movingPoint) return;
+
+        Gizmos.DrawLine(pointA.position, pointB.position);
+        movingPoint.transform.position =
+            Vector3.Project(movingPoint.transform.position - pointA.position, pointB.position - pointA.position) + pointA.position;
+    }
+}
