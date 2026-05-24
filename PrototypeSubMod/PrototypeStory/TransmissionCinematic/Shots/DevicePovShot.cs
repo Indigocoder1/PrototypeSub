1f2417a41f2ca8ddeea6ab68bf91de010b4e76aa
Indigocoder1 <130301845+Indigocoder1@users.noreply.github.com>
2026-05-24 10:04:34 -0700

    Add POV shot to transmission cinematic

---
+++ PrototypeSubMod/PrototypeStory/TransmissionCinematic/Shots/DevicePovShot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PrototypeSubMod.PrototypeStory.TransmissionCinematic.Shots;
+
+public class DevicePovShot : CinematicShot
+{
+    public override event Action<DeviceCinematicManager> OnShotCompleted;
+
+    public void EndPovShot()
+    {
+        OnShotCompleted?.Invoke(deviceCinematicManager);
+    }
+}
