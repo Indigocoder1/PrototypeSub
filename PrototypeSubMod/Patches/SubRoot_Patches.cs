107dabb05d749bb5fb1daf0fccdaed57367aaa22
Indigocoder1 <130301845+Indigocoder1@users.noreply.github.com>
2026-05-27 10:37:06 -0700

    Make sub entry voicelines not be added to queue

--- PrototypeSubMod/Patches/SubRoot_Patches.cs
+++ PrototypeSubMod/Patches/SubRoot_Patches.cs
@@ -1,4 +1,9 @@
-﻿using HarmonyLib;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
 using PrototypeSubMod.MiscMonobehaviors.SubSystems;
 using PrototypeSubMod.MotorHandler;
 
@@ -22,6 +27,30 @@ internal class SubRoot_Patches
         if (!__instance.voiceNotificationManager) return;
         
         __instance.voiceNotificationManager.ClearQueue();
+    }
+    
+    [HarmonyPatch(nameof(SubRoot.OnPlayerEntered)), HarmonyTranspiler]
+    private static IEnumerable<CodeInstruction> OnPlayerEnter_Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        var playNotificationMethod =
+            typeof(VoiceNotificationManager).GetMethod("PlayVoiceNotification", AccessTools.all);
+
+        var match = new CodeMatch[]
+        {
+            new(i => i.opcode == OpCodes.Ldc_I4_1),
+            new(i => i.opcode == OpCodes.Ldc_I4_0),
+            new(i => i.opcode == OpCodes.Callvirt && (MethodInfo)i.operand == playNotificationMethod)
+        };
+
+        var matcher = new CodeMatcher(instructions)
+            .MatchForward(false, match)
+            .SetInstruction(new CodeInstruction(OpCodes.Ldc_I4_0))
+            .MatchForward(false, match)
+            .SetInstruction(new CodeInstruction(OpCodes.Ldc_I4_0))
+            .MatchForward(false, match)
+            .SetInstruction(new CodeInstruction(OpCodes.Ldc_I4_0));
+        
+        return matcher.InstructionEnumeration();
     }
 
     [HarmonyPatch(nameof(SubRoot.UpdateLighting)), HarmonyPrefix]
