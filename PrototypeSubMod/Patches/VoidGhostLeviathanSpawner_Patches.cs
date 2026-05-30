9d25c35305618a31422c8fd1de2c59cebacfd6ca
Indigocoder1 <130301845+Indigocoder1@users.noreply.github.com>
2026-05-30 14:05:03 -0700

    Add extra check to make ghosts not spawn in void

--- PrototypeSubMod/Patches/VoidGhostLeviathanSpawner_Patches.cs
+++ PrototypeSubMod/Patches/VoidGhostLeviathanSpawner_Patches.cs
@@ -6,6 +6,14 @@ namespace PrototypeSubMod.Patches;
 [HarmonyPatch(typeof(VoidGhostLeviathansSpawner))]
 public class VoidGhostLeviathanSpawnerPatch
 {
+    [HarmonyPatch(nameof(VoidGhostLeviathansSpawner.UpdateSpawn))]
+    [HarmonyPrefix]
+    private static bool UpdateSpawn_Prefix()
+    {
+        StoryGoalManager storyGoalManager = StoryGoalManager.main;
+        return !storyGoalManager.IsGoalComplete("HullFacilityWormTerminalEncy");
+    }
+    
     [HarmonyPatch(nameof(VoidGhostLeviathansSpawner.IsPlayerInVoid))]
     [HarmonyPrefix]
     private static bool IsPlayerInVoid_Prefix()
