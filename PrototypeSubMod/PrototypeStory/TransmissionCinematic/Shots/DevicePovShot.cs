3a5837e99c76d091a62e726995c6d37ebad77ca1
Indigocoder1 <130301845+Indigocoder1@users.noreply.github.com>
2026-05-30 14:18:11 -0700

    Add new POV shot video and SFX

--- PrototypeSubMod/PrototypeStory/TransmissionCinematic/Shots/DevicePovShot.cs
+++ PrototypeSubMod/PrototypeStory/TransmissionCinematic/Shots/DevicePovShot.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using UnityEngine;
 
 namespace PrototypeSubMod.PrototypeStory.TransmissionCinematic.Shots;
 
 public class DevicePovShot : CinematicShot
 {
+    [SerializeField] private FMOD_CustomEmitter deviceSfx;
+    
     public override event Action<DeviceCinematicManager> OnShotCompleted;
+
+    public override void PlayShot(Animator animator, DeviceCinematicManager cinematicManager)
+    {
+        base.PlayShot(animator, cinematicManager);
+        deviceSfx.Play();
+    }
 
     public void EndPovShot()
     {
