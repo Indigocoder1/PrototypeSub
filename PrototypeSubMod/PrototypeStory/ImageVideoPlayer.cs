5624570bf2a0cf0dc3fe6d20c694d2fc37208ec1
Indigocoder1 <130301845+Indigocoder1@users.noreply.github.com>
2026-05-31 21:37:22 -0700

    Add new POV shot

---
+++ PrototypeSubMod/PrototypeStory/ImageVideoPlayer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace PrototypeSubMod.PrototypeStory;
+
+public class ImageVideoPlayer : MonoBehaviour
+{
+    [SerializeField] private Image videoImage;
+    [SerializeField] private Sprite[] images;
+    [SerializeField] private int fps;
+
+    private float timer;
+    private float secondsBetweenFrames;
+    private int imageIndex;
+
+    private void Start()
+    {
+        secondsBetweenFrames = 1f / fps;
+        videoImage.sprite = images[imageIndex];
+    }
+
+    private void Update()
+    {
+        if (imageIndex >= images.Length) return;
+        
+        timer += Time.deltaTime;
+        var targetImageIndex = Mathf.FloorToInt(timer / secondsBetweenFrames);
+        
+        if (targetImageIndex == imageIndex) return;
+        
+        imageIndex = targetImageIndex;
+        if (imageIndex >= images.Length) return;
+        
+        videoImage.sprite = images[imageIndex];
+    }
+}
