ec80a6914a89b12d47c522bfdbf2eba67e39118d
OctolinkG <159560650+OctolinkG@users.noreply.github.com>
2026-06-02 14:12:52 +0200

    Decrease charged almanite craft time

--- PrototypeSubMod/Prefabs/AlienBuildingBlock/AlienBuildingBlock.cs
+++ PrototypeSubMod/Prefabs/AlienBuildingBlock/AlienBuildingBlock.cs
@@ -24,7 +24,7 @@ internal class AlienBuildingBlock : RelicBlock
         
         prefab.SetGameObject(GetPrefab);
         
-        prefab.SetRecipe(ROTACompatManager.GetRelevantRecipe("AlienBuildingBlock.json")).WithCraftingTime(3f);
+        prefab.SetRecipe(ROTACompatManager.GetRelevantRecipe("AlienBuildingBlock.json")).WithCraftingTime(1f);
         prefab.SetPdaGroupCategory(Plugin.ProtoFabricatorGroup, Plugin.ProtoFabricatorCatgeory);
 
         CraftDataHandler.SetEatingSound(prefabInfo.TechType, "event:/player/use_first_aid");
