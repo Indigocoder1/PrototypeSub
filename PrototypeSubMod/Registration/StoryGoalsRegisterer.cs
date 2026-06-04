66034996c9fed4c14c4b09865c0a6357df86251b
Indigocoder1 <130301845+Indigocoder1@users.noreply.github.com>
2026-06-03 18:45:05 -0700

    Fix grand reef wyrm ping not saving

--- PrototypeSubMod/Registration/StoryGoalsRegisterer.cs
+++ PrototypeSubMod/Registration/StoryGoalsRegisterer.cs
@@ -435,10 +435,10 @@ internal static class StoryGoalsRegisterer
 
         #region Extra worms
 
+        var grandReefPing = CustomPing.CreatePing("ProtoGrandReefPing", Plugin.HintPingType);
         StoryGoalHandler.RegisterCustomEvent("OnScannedSparseReefWyrms", () =>
         {
             PDALog.Add("OnSparseReefWyrmScanned");
-            var grandReefPing = CustomPing.CreatePing("ProtoGrandReefPing", Plugin.HintPingType);
             UWE.CoroutineHost.StartCoroutine(
                 PuzzleHintRegistration.SpawnPrefab(grandReefPing, new Vector3(-1147, -445, -1110)));
         });
