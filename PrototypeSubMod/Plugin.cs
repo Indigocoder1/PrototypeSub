5eaad923ed42489a4789762f6cf856c900fdbcf8
Indigocoder1 <130301845+Indigocoder1@users.noreply.github.com>
2026-07-17 11:39:21 -0700

    Increase version

--- PrototypeSubMod/Plugin.cs
+++ PrototypeSubMod/Plugin.cs
@@ -54,7 +54,7 @@ namespace PrototypeSubMod
     {
         private const string GUID = "com.prototech.prototypesub";
         private const string pluginName = "Prototype Sub";
-        private const string versionString = "2.0.4";
+        private const string versionString = "2.0.5";
 
         public new static ManualLogSource Logger { get; private set; }
 
